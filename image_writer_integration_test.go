4a161db22e007a01b3899c3dd6215f38c304d420
Kamil Domański <kamil@domanski.co>
2022-05-29 16:29:58 +0200

    os-specific mount

--- image_writer_integration_test.go
+++ image_writer_integration_test.go
@@ -16,6 +16,54 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func osDependentMountCmd(t *testing.T, filename string) (string, func()) {
+	t.Helper()
+	// Mount-DiskImage -ImagePath "E:\ISO-Files\My US Visit Fall 2010 Pictures.iso"
+	switch runtime.GOOS {
+	case "windows":
+		ps, err := exec.LookPath("powershell.exe")
+		assert.NoError(t, err)
+
+		cmd := exec.Command(ps, "-NonInteractive", fmt.Sprintf("Mount-DiskImage -Verbose -ImagePath %q", filename))
+		output, err := cmd.CombinedOutput()
+		assert.NoError(t, err, "failed to mount the ISO image: %v\n%s", err, string(output))
+
+		cleanup := func() {
+			// execute umount
+			umountCmd := exec.Command("Dismount-DiskImage", "-ImagePath", filename)
+			output, err = umountCmd.CombinedOutput()
+			assert.NoError(t, err, "failed to unmount the ISO image: %v\n%s", err, string(output))
+		}
+
+		// We're assuming that the assigned letter will be D:
+		return `D:\\`, cleanup
+	case "linux":
+		// create mount directory
+		mountDir, err := ioutil.TempDir("", "")
+		assert.NoError(t, err)
+
+		cmd := exec.Command("mount", "-t", "iso9660", filename, mountDir)
+		output, err := cmd.CombinedOutput()
+		assert.NoError(t, err, "failed to mount the ISO image: %v\n%s", err, string(output))
+
+		cleanup := func() {
+			// execute umount
+			umountCmd := exec.Command("umount", mountDir)
+			output, err = umountCmd.CombinedOutput()
+			assert.NoError(t, err, "failed to unmount the ISO image: %v\n%s", err, string(output))
+
+			if removeErr := os.RemoveAll(mountDir); removeErr != nil {
+				t.Fatalf("failed to delete mount directory: %v", removeErr)
+			}
+		}
+
+		return mountDir, cleanup
+	}
+
+	t.Fatalf("unknown OS %s", runtime.GOOS)
+	return "", nil
+}
+
 // Identical to TestWriterAndMountWithAddLocal, except it uses
 // AddFile to add files with a constant string in them.
 func TestWriterAndMount(t *testing.T) {
@@ -51,19 +99,8 @@ func TestWriterAndMount(t *testing.T) {
 	// Image mounting
 	//
 
-	// create mount directory
-	mountDir, err := ioutil.TempDir("", "")
-	assert.NoError(t, err)
-	defer func() {
-		if removeErr := os.RemoveAll(mountDir); removeErr != nil {
-			t.Fatalf("failed to delete mount directory: %v", removeErr)
-		}
-	}()
-
-	// execute mount
-	mountCmd := exec.Command("mount", f.Name(), mountDir)
-	output, err := mountCmd.CombinedOutput()
-	assert.NoError(t, err, "failed to mount the ISO image: %v\n%s", err, string(output))
+	mountDir, cleanup := osDependentMountCmd(t, f.Name())
+	defer cleanup()
 
 	// make sure that we can see the 1000 files
 	txtFileCount := 0
@@ -88,10 +125,6 @@ func TestWriterAndMount(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, 1000, txtFileCount)
 
-	// execute umount
-	umountCmd := exec.Command("umount", mountDir)
-	output, err = umountCmd.CombinedOutput()
-	assert.NoError(t, err, "failed to unmount the ISO image: %v\n%s", err, string(output))
 }
 
 // Identical to TestWriterAndMount, except it uses
@@ -143,19 +176,9 @@ func TestWriterAndMountWithAddLocal(t *testing.T) {
 	// Image mounting
 	//
 
-	// create mount directory
-	mountDir, err := ioutil.TempDir("", "")
-	assert.NoError(t, err)
-	defer func() {
-		if removeErr := os.RemoveAll(mountDir); removeErr != nil {
-			t.Fatalf("failed to delete mount directory: %v", removeErr)
-		}
-	}()
-
 	// execute mount
-	mountCmd := exec.Command("mount", f.Name(), mountDir)
-	output, err := mountCmd.CombinedOutput()
-	assert.NoError(t, err, "failed to mount the ISO image: %v\n%s", err, string(output))
+	mountDir, cleanup := osDependentMountCmd(t, f.Name())
+	defer cleanup()
 
 	// make sure that we can see the 1000 files
 	txtFileCount := 0
@@ -179,9 +202,4 @@ func TestWriterAndMountWithAddLocal(t *testing.T) {
 	err = filepath.Walk(mountDir, walkfn)
 	assert.NoError(t, err)
 	assert.Equal(t, 1000, txtFileCount)
-
-	// execute umount
-	umountCmd := exec.Command("umount", mountDir)
-	output, err = umountCmd.CombinedOutput()
-	assert.NoError(t, err, "failed to unmount the ISO image: %v\n%s", err, string(output))
 }
